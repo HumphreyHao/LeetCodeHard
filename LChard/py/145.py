5a1fcf8a536beb5e60e2c73dc70e52ec60073c19
humphreyHao <haz013@ucsd.edu>
2020-01-05 15:20:11 -0800

    154

--- LChard/py/145.py
+++ LChard/py/145.py
@@ -1,3 +1,4 @@
+from collections import deque
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -9,8 +10,23 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
-        if not root:
-            return []
-        return self.postorderTraversal(root.left)+self.postorderTraversal(root.right)+[root.val]
-        
+        res=[]
+        queue=deque()
+        lastVisit=root
+        while root or queue:
+            while root:
+                queue.append(root)
+                root=root.left
+            #先一直往左找，因为先返回的是左
+            root =queue[-1]
+            if root.right is None or root.right == lastVisit:
+                res.append(root.val)
+                queue.pop()
+                lastVisit=root
+                root=None
+                #对当前节点判断一下，是该往上走了还是该往右走了，如果
+            else:
+                root=root.right
+        return res
+            
         
